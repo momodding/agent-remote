47c2c7736d6c08ef5fe6b7e0c259776d094c5d98
momodding <utsman.fajar@gmail.com>
2026-08-07 13:02:57 +0700

    omp(new-session-glass-sheet): Run typecheck and test suite

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   ...expoPreset,
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   resolver: 'react-native-worklets/jest/resolver',
+  moduleNameMapper: {
+    '^@gorhom/bottom-sheet$': '<rootDir>/node_modules/@gorhom/bottom-sheet/mock.js',
+  },
   transformIgnorePatterns: [
     '/node_modules/(?!(.pnpm|react-native|@react-native|@react-native-community|expo|@expo|@expo-google-fonts|react-navigation|@react-navigation|@sentry/react-native|native-base|standard-navigation|@noble/hashes))',
     '/node_modules/react-native-reanimated/plugin/',
